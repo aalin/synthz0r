e320aea93874b2019800ca6cef7c33f29f1b4ad2
Andreas Alin <andreas.alin@gmail.com>
2020-06-17 22:25:56 -0500

    Increase buffer size

--- src/application.cpp
+++ src/application.cpp
@@ -12,7 +12,7 @@
 
 #include "file_output.hpp"
 
-constexpr unsigned int BUFFER_SIZE = 1024 * 1;
+constexpr unsigned int BUFFER_SIZE = 1024 * 8;
 constexpr unsigned int SAMPLE_RATE = 44100;
 constexpr unsigned int NUM_CHANNELS = 2;
 constexpr int DEFAULT_PORT = 9000;
