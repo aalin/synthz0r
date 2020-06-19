2b0e20d09e2b65347a68f6819c9eb10269a2c670
Andreas Alin <andreas.alin@gmail.com>
2020-06-19 07:25:09 -0500

    Set default envelope parameters

--- src/devices/instruments/wavetable_synth.hpp
+++ src/devices/instruments/wavetable_synth.hpp
@@ -42,9 +42,9 @@ namespace Devices::Instruments {
 				Parameter("panning",            -127,                        127,   0, _panning),
 				Parameter("amplitude",             0,                        128, 100, _amplitude),
 				Parameter("envelope.attackMs",     0,                       1000, 150, _envelope._attackMs),
-				Parameter("envelope.decayMs",      0,                       1000,   0, _envelope._decayMs),
-				Parameter("envelope.sustain",      0,                        127,   0, _envelope._sustain),
-				Parameter("envelope.releaseMs",    0,                       1000,   0, _envelope._releaseMs),
+				Parameter("envelope.decayMs",      0,                       1000,  60, _envelope._decayMs),
+				Parameter("envelope.sustain",      0,                        127,  30, _envelope._sustain),
+				Parameter("envelope.releaseMs",    0,                       1000,  70, _envelope._releaseMs),
 				Parameter("waveformIndex",         0, Waveform::WAVEFORMS.size()-1, 0, _waveformIndex)
 			  })
 			{}
