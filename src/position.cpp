6ab055bab403527bb716733fbc5f0fab5fcb68e0
Andreas Alin <andreas.alin@gmail.com>
2020-06-26 21:55:03 -0500

    Add Postion::parse(str)

--- src/position.cpp
+++ src/position.cpp
@@ -1,13 +1,38 @@
 #include "position.hpp"
 #include <iostream>
+#include <regex>
+
+uint64_t Position::parse(const std::string &str) {
+	const std::regex re("^(\\d+)\\.(\\d+)\\.(\\d+)\\.(\\d+)$");
+	std::smatch match;
+
+	if (!std::regex_match(str, match, re)) {
+		throw std::runtime_error("Could not parse string");
+	}
+
+	const uint32_t bar = atoi(match.str(1).c_str());
+	const uint8_t beat = atoi(match.str(2).c_str());
+	const uint8_t sixteenths = atoi(match.str(3).c_str());
+	const uint8_t ticks = atoi(match.str(4).c_str());
+
+	return ticksFromPosition(bar, beat, sixteenths, ticks);
+}
+
+std::string Position::toString() const {
+	std::ostringstream oss;
+
+	oss <<
+		"Position(" <<
+		(uint32_t)bar << "." <<
+		(uint32_t)beat << "." <<
+		(uint32_t)sixteenths << "." <<
+		(uint32_t)ticks << ")";
+
+	return oss.str();
+}
 
 std::ostream & operator<<(std::ostream &out, const Position &position) {
-	return out <<
-		"Position(" <<
-		(int)position.bar << "." <<
-		(int)position.beat << "." <<
-		(int)position.sixteenths << "." <<
-		(int)position.ticks << ")";
+	return out << position.toString();
 }
 
 void Position::update(const float &bpm, const float &sampleRate) {
