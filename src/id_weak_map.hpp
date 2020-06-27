f6b72b9d268be391d7042f15a23ffb0cb8d0ba80
Andreas Alin <andreas.alin@gmail.com>
2020-06-27 08:22:00 -0500

    Add a simple map that holds weak pointers

---
+++ src/id_weak_map.hpp
@@ -0,0 +1,40 @@
+#ifndef ID_WEAK_MAP_HPP
+#define ID_WEAK_MAP_HPP
+
+template<typename T>
+class IdWeakMap {
+	public:
+		void add(std::shared_ptr<T> objectPtr) {
+			_map[objectPtr->id()] = objectPtr;
+			cleanExpired();
+		}
+
+		std::shared_ptr<T> get(uint32_t id) {
+			cleanExpired();
+
+			auto it = _map.find(id);
+
+			if (it == _map.end()) {
+				return nullptr;
+			}
+
+			return it->second.lock();
+		}
+
+	private:
+		std::map<uint32_t, std::weak_ptr<T> > _map;
+
+		void cleanExpired() {
+			auto it = _map.begin();
+
+			while (it != _map.end()) {
+				if (it->second.expired()) {
+					it = _map.erase(it);
+				} else {
+					it++;
+				}
+			}
+		}
+};
+
+#endif
